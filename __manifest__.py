c7691a1b641c71f3f1eb99df6891fb3d84e102fc
Diego Gajardo <diegobgajardo@gmail.com>
2025-02-18 14:32:24 -0300

    .

--- __manifest__.py
+++ __manifest__.py
@@ -7,7 +7,7 @@
     'data': [
         'views/website_snippet_templates.xml',
         'views/vehicle_views.xml',
-        'views/assets.xml',
+       # 'views/assets.xml',
 
     ],
     'assets': {
