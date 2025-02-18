01699a78cdccdf26d7e7d0bb0bbf94fda3c95aec
Diego Gajardo <diegobgajardo@gmail.com>
2025-02-18 12:28:13 -0300

    update 2

--- __manifest__.py
+++ __manifest__.py
@@ -13,8 +13,10 @@
         'web.assets_frontend': [
             'cars_dealersheep/static/src/js/vehicle_snippet.js',
             'cars_dealersheep/static/src/xml/vehicle_snippet.xml',
+            'cars_dealersheep/static/src/scss/theme.scss',  # Agregado el scss
         ],
     },
+
     'installable': True,
     'application': False,
 }
