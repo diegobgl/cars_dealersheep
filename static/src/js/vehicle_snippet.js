ebaa285b245ba851ad1519e58fd3faa79a194cb2
Diego Gajardo <diegobgajardo@gmail.com>
2025-02-24 18:12:20 -0300

    kk

--- static/src/js/vehicle_snippet.js
+++ static/src/js/vehicle_snippet.js
@@ -7,6 +7,33 @@ odoo.define('cars_dealersheep.vehicle_snippet', function(require) {
         selector: '.vehicle_snippet',
         start: function() {
             console.log("Snippet de Vehículos Cargado!");
+            var self = this;
+            rpc.query({
+                route: '/vehicles/json',
+                type: 'json',
+            }).then(function(vehicles) {
+                var html = '';
+                if (vehicles.length > 0) {
+                    vehicles.forEach(function(vehicle) {
+                        html += '<div class="vehicle-item">' +
+                                    '<div class="card">' +
+                                        '<img src="/web/image/vehicle.vehicle/' + (vehicle.id|string) + '/image" class="card-img-top" alt="' + vehicle.name + '"/>' +
+                                        '<div class="card-body">' +
+                                            '<h5>' + vehicle.name + '</h5>' +
+                                            '<p>Marca: ' + vehicle.brand + '</p>' +
+                                            '<p>Modelo: ' + (vehicle.model || '') + '</p>' +
+                                            '<p>Año: ' + (vehicle.year || '') + '</p>' +
+                                            '<p>Precio: ' + vehicle.price + ' USD</p>' +
+                                            '<p>Estado: ' + vehicle.status + '</p>' +
+                                        '</div>' +
+                                    '</div>' +
+                                '</div>';
+                    });
+                } else {
+                    html = '<p>No hay vehículos disponibles.</p>';
+                }
+                self.$el.find('.vehicle-list').html(html);
+            });
         }
     });
 
