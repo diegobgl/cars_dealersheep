7bb5e19eae9145b9bd2e5047f17c5f9cb7b6d97a
Diego Gajardo <diegobgajardo@gmail.com>
2025-02-25 11:23:55 -0300

    new version

--- static/src/js/vehicle_snippet.js
+++ static/src/js/vehicle_snippet.js
@@ -1,5 +1,6 @@
 odoo.define('cars_dealersheep.vehicle_snippet', function(require) {
     "use strict";
+
     var PublicWidget = require('web.public.widget');
     var rpc = require('web.rpc');
 
@@ -8,6 +9,8 @@ odoo.define('cars_dealersheep.vehicle_snippet', function(require) {
         start: function() {
             console.log("Snippet de Vehículos Cargado!");
             var self = this;
+
+            // Llamada al endpoint que devuelve el JSON de vehículos
             rpc.query({
                 route: '/vehicles/json',
                 type: 'json',
@@ -15,17 +18,18 @@ odoo.define('cars_dealersheep.vehicle_snippet', function(require) {
                 var html = '';
                 if (vehicles.length > 0) {
                     vehicles.forEach(function(vehicle) {
+                        // ID, brand, model, year, price, status, name, etc.
                         html += '<div class="vehicle-item">' +
                                     '<div class="card">' +
-                                        '<img src="/web/image/vehicle.vehicle/' + (vehicle.id|string) + '/image" ' +
-                                             'class="card-img-top" alt="' + vehicle.name + '"/>' +
+                                        '<img src="/web/image/vehicle.vehicle/' + vehicle.id + '/image" ' +
+                                             'class="card-img-top" alt="' + (vehicle.name || '') + '"/>' +
                                         '<div class="card-body">' +
-                                            '<h5>' + vehicle.name + '</h5>' +
-                                            '<p>Marca: ' + vehicle.brand + '</p>' +
+                                            '<h5>' + (vehicle.name || '') + '</h5>' +
+                                            '<p>Marca: ' + (vehicle.brand || '') + '</p>' +
                                             '<p>Modelo: ' + (vehicle.model || '') + '</p>' +
                                             '<p>Año: ' + (vehicle.year || '') + '</p>' +
-                                            '<p>Precio: ' + vehicle.price + ' USD</p>' +
-                                            '<p>Estado: ' + vehicle.status + '</p>' +
+                                            '<p>Precio: ' + (vehicle.price || '') + ' USD</p>' +
+                                            '<p>Estado: ' + (vehicle.status || '') + '</p>' +
                                         '</div>' +
                                     '</div>' +
                                 '</div>';
@@ -33,10 +37,16 @@ odoo.define('cars_dealersheep.vehicle_snippet', function(require) {
                 } else {
                     html = '<p>No hay vehículos disponibles.</p>';
                 }
-                // Sustitución del contenido de .vehicle-list
+                // Rellenar el contenido del snippet
                 self.$el.find('.vehicle-list').html(html);
+            })
+            .catch(function(err) {
+                console.error("Error RPC /vehicles/json:", err);
+                self.$el.find('.vehicle-list').html(
+                    '<div class="alert alert-danger">Error al cargar los vehículos.</div>'
+                );
             });
-        }
+        },
     });
 
     PublicWidget.registry.vehicle_snippet = VehicleSnippet;
