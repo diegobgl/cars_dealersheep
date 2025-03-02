d62599734bb6a7c58d1ebb5a3d2e12726c119254
Diego Gajardo <diegobgajardo@gmail.com>
2025-03-02 17:14:10 -0300

    js actualizado

--- static/src/js/vehicle_snippet.js
+++ static/src/js/vehicle_snippet.js
@@ -14,20 +14,24 @@ publicWidget.registry.VehicleSnippet = publicWidget.Widget.extend({
             if (vehicles.length > 0) {
                 vehicles.forEach(function(vehicle) {
                     html += `<div class="vehicle-item col-md-4 mb-3">
-                        <div class="card vehicle-card">
-                            <img src="/web/image/vehicle.vehicle/${vehicle.id}/image" class="card-img-top" alt="${vehicle.name || ''}">
-                            <div class="card-body">
-                                <h5 class="card-title">${vehicle.name || ''}</h5>
-                                <p class="card-text"><i class="fa fa-car"></i> <strong>Marca:</strong> ${vehicle.brand || ''}</p>
-                                <p class="card-text"><i class="fa fa-cogs"></i> <strong>Modelo:</strong> ${vehicle.model || ''}</p>
-                                <p class="card-text"><i class="fa fa-calendar"></i> <strong>Año:</strong> ${vehicle.year || ''}</p>
-                                <p class="card-text"><i class="fa fa-money"></i> <strong>Precio:</strong> ${vehicle.price || ''} USD</p>
-                                <p class="card-text"><i class="fa fa-info-circle"></i> <strong>Estado:</strong> ${vehicle.status || ''}</p>
-                                <p class="card-text"><i class="fa fa-tag"></i> <strong>Tipo:</strong> ${vehicle.vehicle_type || ''}</p>`;
+                        <a href="/vehicle/${vehicle.id}" class="text-decoration-none">
+                          <div class="card vehicle-card">
+                              <img src="/web/image/vehicle.vehicle/${vehicle.id}/image" class="card-img-top" alt="${vehicle.name || ''}">
+                              <div class="card-body">
+                                  <h5 class="card-title">${vehicle.name || ''}</h5>
+                                  <p class="card-text"><i class="fa fa-car"></i> <strong>Marca:</strong> ${vehicle.brand || ''}</p>
+                                  <p class="card-text"><i class="fa fa-cogs"></i> <strong>Modelo:</strong> ${vehicle.model || ''}</p>
+                                  <p class="card-text"><i class="fa fa-calendar"></i> <strong>Año:</strong> ${vehicle.year || ''}</p>
+                                  <p class="card-text"><i class="fa fa-money"></i> <strong>Precio:</strong> ${vehicle.price || ''} USD</p>
+                                  <p class="card-text"><i class="fa fa-info-circle"></i> <strong>Estado:</strong> ${vehicle.status || ''}</p>
+                                  <p class="card-text"><i class="fa fa-tag"></i> <strong>Tipo:</strong> ${vehicle.vehicle_type || ''}</p>`;
                     if (vehicle.description) {
                         html += `<p class="card-text"><i class="fa fa-align-left"></i> <strong>Descripción:</strong> ${vehicle.description}</p>`;
                     }
-                    html += `</div></div></div>`;
+                    html += `          </div>
+                          </div>
+                        </a>
+                      </div>`;
                 });
             } else {
                 html = '<p>No hay vehículos disponibles.</p>';
