72f9b1d3b54e96f1230207adb7ece4d60d2d4173
Diego Gajardo <diegobgajardo@gmail.com>
2025-03-02 17:33:53 -0300

    actualizacion js sccc

--- static/src/js/single_vehicle_snippet.js
+++ static/src/js/single_vehicle_snippet.js
@@ -5,29 +5,27 @@ import publicWidget from 'web.public.widget';
 publicWidget.registry.SingleVehicleSnippet = publicWidget.Widget.extend({
     selector: '.s_single_vehicle_snippet',
     start() {
-        console.log("Single Vehicle Snippet Cargado");
-        const imgEl = this.el.querySelector('img');
-        const titleEl = this.el.querySelector('h2');
-        const pEl = this.el.querySelector('p');
-
-        this._rpc({
+        const self = this;
+        // Llamada RPC para obtener vehículos
+        self._rpc({
             route: '/vehicles/json',
             params: {},
-        }).then(vehicles => {
+        }).then(function(vehicles) {
             if (vehicles.length > 0) {
-                const vehicle = vehicles[0]; // Puedes personalizar la lógica
-                if (imgEl) {
-                    imgEl.setAttribute('src', '/web/image/vehicle.vehicle/' + vehicle.id + '/image');
-                }
-                if (titleEl) {
-                    titleEl.textContent = vehicle.name;
-                }
-                if (pEl) {
-                    pEl.textContent = `Modelo: ${vehicle.model || ''} - Año: ${vehicle.year || ''}`;
-                }
+                const vehicle = vehicles[0]; // Se muestra el primer vehículo
+                // Actualiza el contenido de la card mediante jQuery
+                const $card = self.$el.find('.vehicle-card');
+                $card.find('.card-img-top').attr('src', '/web/image/vehicle.vehicle/' + vehicle.id + '/image');
+                $card.find('.card-title').text(vehicle.name);
+                // Actualizamos los campos utilizando jQuery para cada párrafo
+                $card.find('p.card-text').each(function() {
+                    const $p = $(this);
+                    // El contenido se renderiza ya en el template, pero si se requiere
+                    // se puede actualizar dinámicamente aquí.
+                });
             }
-        }).catch(err => {
-            console.error("Error en RPC /vehicles/json:", err);
+        }).catch(function(err) {
+            console.error("Error RPC /vehicles/json:", err);
         });
     },
 });
