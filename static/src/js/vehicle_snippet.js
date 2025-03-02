73fd0cfb0c753c63ab226f3600f4d980d958f33f
Diego Gajardo <diegobgajardo@gmail.com>
2025-03-02 17:07:01 -0300

    new v2

--- static/src/js/vehicle_snippet.js
+++ static/src/js/vehicle_snippet.js
@@ -5,42 +5,38 @@ import publicWidget from 'web.public.widget';
 publicWidget.registry.VehicleSnippet = publicWidget.Widget.extend({
     selector: '.s_vehicle_snippet',
     start() {
-        console.log("Snippet de Vehículos Cargado!");
-        const vehicleList = this.el.querySelector('.vehicle-list');
-        if (vehicleList) {
-            this._rpc({
-                route: '/vehicles/json',
-                params: {},
-            }).then(vehicles => {
-                let html = '';
-                if (vehicles.length > 0) {
-                    vehicles.forEach(vehicle => {
-                        html += `<div class="vehicle-item col-md-4 mb-3">
-                            <a href="/vehicle/${vehicle.id}" class="text-decoration-none">
-                                <div class="card vehicle-card">
-                                    <img src="/web/image/vehicle.vehicle/${vehicle.id}/image" class="card-img-top" alt="${vehicle.name || ''}">
-                                    <div class="card-body">
-                                        <h5 class="card-title">${vehicle.name || ''}</h5>
-                                        <p class="card-text">Tipo: ${vehicle.type || ''}</p>
-                                        <p class="card-text">Marca: ${vehicle.brand || ''}</p>
-                                        <p class="card-text">Modelo: ${vehicle.model || ''}</p>
-                                        <p class="card-text">Año: ${vehicle.year || ''}</p>
-                                        <p class="card-text">Precio: ${vehicle.price || ''} USD</p>
-                                        <p class="card-text">Estado: ${vehicle.status || ''}</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>`;
-                    });
-                } else {
-                    html = '<p>No hay vehículos disponibles.</p>';
-                }
-                vehicleList.innerHTML = html;
-            }).catch(err => {
-                console.error("Error RPC /vehicles/json:", err);
-                vehicleList.innerHTML = '<div class="alert alert-danger">Error al cargar los vehículos.</div>';
-            });
-        }
+        const self = this;
+        self._rpc({
+            route: '/vehicles/json',
+            params: {},
+        }).then(function(vehicles) {
+            let html = '';
+            if (vehicles.length > 0) {
+                vehicles.forEach(function(vehicle) {
+                    html += `<div class="vehicle-item col-md-4 mb-3">
+                        <div class="card vehicle-card">
+                            <img src="/web/image/vehicle.vehicle/${vehicle.id}/image" class="card-img-top" alt="${vehicle.name || ''}">
+                            <div class="card-body">
+                                <h5 class="card-title">${vehicle.name || ''}</h5>
+                                <p class="card-text"><i class="fa fa-car"></i> <strong>Marca:</strong> ${vehicle.brand || ''}</p>
+                                <p class="card-text"><i class="fa fa-cogs"></i> <strong>Modelo:</strong> ${vehicle.model || ''}</p>
+                                <p class="card-text"><i class="fa fa-calendar"></i> <strong>Año:</strong> ${vehicle.year || ''}</p>
+                                <p class="card-text"><i class="fa fa-money"></i> <strong>Precio:</strong> ${vehicle.price || ''} USD</p>
+                                <p class="card-text"><i class="fa fa-info-circle"></i> <strong>Estado:</strong> ${vehicle.status || ''}</p>
+                                <p class="card-text"><i class="fa fa-tag"></i> <strong>Tipo:</strong> ${vehicle.vehicle_type || ''}</p>`;
+                    if (vehicle.description) {
+                        html += `<p class="card-text"><i class="fa fa-align-left"></i> <strong>Descripción:</strong> ${vehicle.description}</p>`;
+                    }
+                    html += `</div></div></div>`;
+                });
+            } else {
+                html = '<p>No hay vehículos disponibles.</p>';
+            }
+            self.$el.find('.vehicle-list').html(html);
+        }).catch(function(err) {
+            console.error("Error RPC /vehicles/json:", err);
+            self.$el.find('.vehicle-list').html('<div class="alert alert-danger">Error al cargar los vehículos.</div>');
+        });
     },
 });
 
